14e462e864892c09f36e693b7e7bb6603ebbeef7
Casey Marshall <casey.marshall@memeo-inc.com>
2012-04-11 11:03:22 -0700

    Initial Commit

---
+++ CouchDB/CouchDB.h
@@ -0,0 +1,13 @@
+//
+//  CouchDB.h
+//  CouchDB
+//
+//  Created by Casey Marshall on 4/11/12.
+//  Copyright (c) 2012 Memeo, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CouchDB : NSObject
+
+@end
---
+++ CouchDB/CouchDB.m
@@ -0,0 +1,13 @@
+//
+//  CouchDB.m
+//  CouchDB
+//
+//  Created by Casey Marshall on 4/11/12.
+//  Copyright (c) 2012 Memeo, Inc. All rights reserved.
+//
+
+#import "CouchDB.h"
+
+@implementation CouchDB
+
+@end
