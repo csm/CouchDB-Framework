65ad79f947c3d0379f37cac17c7bf976fdb64e21
Casey Marshall <casey.marshall@memeo-inc.com>
2012-04-13 19:01:37 -0700

    Updates.

--- otp_src_R15B01/build.sh
+++ otp_src_R15B01/build.sh
@@ -12,9 +12,10 @@ test -z "$action" && action=build
 case "$action" in
     build)
         test -f ${BUILT_PRODUCTS_DIR}/erlang/erlang-built && exit 0
-        cd ${SRCROOT}/otp_src_R15B01
+        mkdir -p ${CONFIGURATION_TEMP_DIR}/otp_src_R15B01
+        cd ${CONFIGURATION_TEMP_DIR}/otp_src_R15B01
         rm -rf otp_src_R15B01
-        cp -r ${SRCROOT}/otp_src_R15B01 .
+        cp -r ${SRCROOT}/otp_src_R15B01/ .
         cd otp_src_R15B01
         CFLAGS="-O0" ./otp_src_R15B01/configure --prefix=${BUILT_PRODUCTS_DIR}/erlang --enable-kernel-poll --enable-threads --enable-dynamic-ssl-lib --enable-shared-zlib --enable-smp-support --enable-darwin-64bit --without-javac
         touch lib/wx/SKIP
