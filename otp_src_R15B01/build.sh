8c73ad28e34aa7171fe099e0010c91c882582821
Casey Marshall <casey.s.marshall@gmail.com>
2012-04-15 14:50:30 -0700

    Update launcher scripts. Create empty directories in otp_src_R1501 (required for build; can't put them in git). Update to sample local.ini Update project file.

--- otp_src_R15B01/build.sh
+++ otp_src_R15B01/build.sh
@@ -12,20 +12,24 @@ test -z "$action" && action=build
 case "$action" in
     build)
         test -f ${BUILT_PRODUCTS_DIR}/erlang/erlang-built && exit 0
-        mkdir -p ${CONFIGURATION_TEMP_DIR}/otp_src_R15B01
-        cd ${CONFIGURATION_TEMP_DIR}/otp_src_R15B01
+        mkdir -p ${CONFIGURATION_TEMP_DIR}
+        cd ${CONFIGURATION_TEMP_DIR}
         rm -rf otp_src_R15B01
-        cp -r ${SRCROOT}/otp_src_R15B01/ .
+        cp -r ${SRCROOT}/otp_src_R15B01 ${CONFIGURATION_TEMP_DIR}
         cd otp_src_R15B01
-        CFLAGS="-O0" ./otp_src_R15B01/configure --prefix=${BUILT_PRODUCTS_DIR}/erlang --enable-kernel-poll --enable-threads --enable-dynamic-ssl-lib --enable-shared-zlib --enable-smp-support --enable-darwin-64bit --without-javac
+        for dir in `cat ${SRCROOT}/otp_src_R15B01/empty.dirs`
+        do
+            mkdir -p $dir
+        done
+        CFLAGS="-O0" ./configure --prefix=${BUILT_PRODUCTS_DIR}/erlang --enable-kernel-poll --enable-threads --enable-dynamic-ssl-lib --enable-shared-zlib --enable-smp-support --enable-darwin-64bit --without-javac --disable-hipe
         touch lib/wx/SKIP
-        make -j8
+        make
         make install
         touch ${BUILT_PRODUCTS_DIR}/erlang/erlang-built
     ;;
 
     clean)
-        make -C ${SRCROOT}/otp_src_R15B01 clean
+        rm -rf ${CONFIGURATION_TEMP_DIR}/otp_src_R15B01
         rm -rf ${BUILT_PRODUCTS_DIR}/erlang
     ;;
 
