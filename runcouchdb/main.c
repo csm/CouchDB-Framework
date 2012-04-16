e69fe091376c80f6c6027fad3df2fe28345cc91a
Casey Marshall <casey.s.marshall@gmail.com>
2012-04-15 19:12:47 -0700

    Add C launcher for couchdb.

---
+++ runcouchdb/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  runcouchdb
+//
+//  Created by Casey Marshall on 4/15/12.
+//  Copyright (c) 2012 Memeo, Inc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
